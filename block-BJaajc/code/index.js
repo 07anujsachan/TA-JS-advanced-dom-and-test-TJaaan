e4ea2c9a7f1ed59401cec50607f3e644ac77335a
anuj sachan <anujsachan98@gmail.com>
2023-05-26 16:50:47 +0530

    Writing Test Using Jest

---
+++ block-BJaajc/code/index.js
@@ -0,0 +1,25 @@
+function getFullName(firstName, lastName) {
+  return '${"firstName"}  ${"lastName"}';
+}
+function isPalindrome(str) {
+  let length = str.length - 1;
+  for (let i = 0; i < length / 2; i++) {
+    let x = str[i];
+    let y = str[length - i];
+    if (x != y) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+function getCircumference(radius) {
+  let circumference = 2 * 3.14 * radius;
+  return `The circumference is ${circumference}`;
+}
+function getArea(radius) {
+  let area = 3.14 * radius * radius;
+  return `The area is ${area}`;
+}
+
+module.exports = getFullName , isPalindrome , getCircumference, getArea;
