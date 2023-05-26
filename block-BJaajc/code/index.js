d5317a4cb4be2a9f7ee05a37967d5c5c0e9d62b0
anuj sachan <anujsachan98@gmail.com>
2023-05-26 17:58:43 +0530

    Writing Test Using Jest

--- block-BJaajc/code/index.js
+++ block-BJaajc/code/index.js
@@ -1,26 +1,8 @@
-function getFullName(firstName, lastName) {
-  let fullName ='${"firstName"}  ${"lastName"}';
-  return fullName
-}
-function isPalindrome(str) {
-  let length = str.length - 1;
-  for (let i = 0; i < length / 2; i++) {
-    let x = str[i];
-    let y = str[length - i];
-    if (x != y) {
-      return false;
-    } else {
-      return true;
-    }
-  }
-}
-function getCircumference(radius) {
-  let circumference = 2 * 3.14 * radius;
-  return `The circumference is ${circumference}`;
-}
-function getArea(radius) {
-  let area = 3.14 * radius * radius;
-  return `The area is ${area}`;
-}
+const getFullName = (firstName, lastName) => {
+  let fullName = `${firstName} ${lastName}`;
+  return fullName;
+};
 
-module.exports = {getFullName , isPalindrome , getCircumference, getArea};
+console.log(getFullName("anuj", "sachan"));
+
+module.exports = getFullName;
